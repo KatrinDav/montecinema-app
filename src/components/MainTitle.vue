4b4f000de9796c2a32071c53350ac8b7bee9ad8b
Katrin Dav <k.dawidowicz2016@gmail.com>
2022-03-02 13:36:02 +0100

    create MainTitle component

---
+++ src/components/MainTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="header-section">{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: "MainTitle",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@media (min-width: 0px) {
+  .header-section {
+    font-family: $ff-secondary;
+    font-size: $fs-large-alt;
+    line-height: 49px;
+    padding: 35px 0;
+  }
+}
+</style>
