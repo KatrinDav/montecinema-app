130d2727ab01a994504b859d828139864fe1f5c3
Katrin Dav <k.dawidowicz2016@gmail.com>
2022-03-03 19:01:32 +0100

    create view for LoginPage

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="login-container">
+    <MainTitle
+      text="Hi there!"
+      subText="Care to log in?"
+      class="login-container__title"
+    />
+    <FormLogin />
+  </main>
+</template>
+
+<script>
+import FormLogin from "../components/FormLogin.vue";
+import MainTitle from "../components/MainTitle.vue";
+export default {
+  name: "LoginPage",
+  components: { FormLogin, MainTitle },
+};
+</script>
+
+<style lang="scss">
+@media (min-width: 0px) {
+}
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 60px;
+  overflow: hidden;
+}
+.login-container__title {
+  width: 600px;
+  text-align: left;
+}
+</style>
