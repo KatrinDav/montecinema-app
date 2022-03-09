bda40149093bc7e9d0dcb6df52c093258686a5ef
Katarzyna Dawidowicz <k.dawidowicz2016@gmail.com>
2022-03-09 18:44:00 +0100

    create component with lists of screening

---
+++ src/components/ScreeningsList.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="list-wrapper">
+        <ScreeningCard
+            v-for="movie in movies"
+             :key="movie.id"
+             :movie="movie"
+        />
+      
+    </section>
+</template>
+
+<script>
+import ScreeningCard from './ScreeningCard.vue';
+import {BASE_URL} from '../helpers/index';
+import axios from 'axios';
+
+export default {
+    name: 'ScreeningsList',
+    components: {ScreeningCard},
+
+    data(){
+        return{
+            movies: [],
+        }
+    },
+
+    methods:{
+    async fetchData(){
+      try{
+         const response = await axios.get(`${BASE_URL}/movies`);
+         console.log(response.data)
+         this.movies = response.data;
+      } catch{
+          this.errorMsg = 'Something went wrong...'
+          console.log(this.errorMsg)
+      }
+     
+    },
+  
+  },
+
+  mounted(){
+    this.fetchData()
+  },
+}
+</script>
+
+<style lang="scss">
+.list-wrapper{
+    width: 100%;
+    margin-top: 40px;
+  
+}
+</style>
