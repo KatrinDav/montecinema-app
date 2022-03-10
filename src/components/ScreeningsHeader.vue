49aa309d872cfb601df62e4305ac521a08cc838b
Katarzyna Dawidowicz <k.dawidowicz2016@gmail.com>
2022-03-10 16:22:33 +0100

    replace methods by computed properties

--- src/components/ScreeningsHeader.vue
+++ src/components/ScreeningsHeader.vue
@@ -2,8 +2,8 @@
     <header>
         <h2 :class="headerClasses">Screenings: <br/>
            <span class="screening-title__date">
-          {{getFullDayName()}}
-          {{ getDayInfo()}}/{{getMonthInfo()}}/{{getYearInfo()}}</span>
+          {{fullDayName}}
+          {{day}}/{{month}}/{{year}}</span>
           </h2>
     </header>
     
@@ -22,11 +22,9 @@ export default {
     data(){
     return {
       daysFullName: ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday', 'Friday', 'Saturday'],
-      day: '',
-      month: '',
-      year: '',
-    }
-  },
+     
+     }
+    },
     computed: {
           headerClasses(){
             return [
@@ -34,37 +32,40 @@ export default {
                 {'screening-title--large': this.sizeType === 'large'},
                 {'screening-title--small': this.sizeType === 'small'},
             ]
-        }
+        },
+
+    day(){
+     const newDate = new Date();
+     const day = newDate.getDate();
+     if(day < 10){
+       return '0'+ day
+     }
+     return day
     },
 
-  methods:{
-    getFullDayName(){
+    month(){
+    const newDate = new Date();
+    const month = newDate.getMonth() +1 ;
+     if(month < 10){
+       return '0' + month
+     }
+     return month
+    },
+
+    year(){
+     const newDate = new Date();
+     const year = newDate.getFullYear();
+     return year;
+    },
+
+    fullDayName(){
     const newDate = new Date();
     return this.daysFullName[newDate.getDay()]
    
     },
-    getDayInfo(){
-     const newDate = new Date();
-     this.day = newDate.getDate();
-     if(this.day < 10){
-       return '0'+ this.day
-     }
-     return this.day
+
     },
-    getMonthInfo(){
-    const newDate = new Date();
-     this.month = newDate.getMonth() +1 ;
-     if(this.month < 10){
-       return '0' + this.month
-     }
-     return this.month
-    },
-    getYearInfo(){
-     const newDate = new Date();
-     this.year = newDate.getFullYear();
-     return this.year
-    }
-  }
+
 }
 </script>
 
@@ -96,8 +97,5 @@ export default {
     }
   }
 }
-
- 
-
  
 </style>
