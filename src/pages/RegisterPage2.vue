12fc07a6bd167f141ea057b371722b48fe3fa3de
Katarzyna Dawidowicz <k.dawidowicz2016@gmail.com>
2022-03-15 17:52:47 +0100

    add new components

---
+++ src/pages/RegisterPage2.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="register-container">
+    <MainTitle
+      text="Great!"
+      subText="Now your name"
+      class="register-container__title"
+    />
+
+    <FormName />
+
+  </main>
+</template>
+
+<script>
+import MainTitle from "../components/MainTitle.vue";
+import FormName from "../components/FormName.vue";
+
+export default {
+  name: "RegisterPage2",
+  components: { MainTitle, FormName },
+};
+</script>
+
+<style lang="scss">
+@media (min-width: 0px) {
+  .register-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 10px;
+    padding-bottom: 60px;
+    overflow: hidden;
+  }
+  .register-container__title {
+    width: 605px;
+    text-align: center;
+  }
+}
+@media (min-width: 932px) {
+  .register-container__title {
+    text-align: left;
+  }
+}
+</style>
